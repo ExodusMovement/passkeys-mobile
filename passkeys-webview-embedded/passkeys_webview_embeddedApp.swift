b599aace2658f86d83dea579e4bfd612b762ec71
Jan W <jan.we@exodus.io>
2024-10-11 09:17:11 +0200

    Initial Commit

---
+++ passkeys-webview-embedded/passkeys_webview_embeddedApp.swift
@@ -0,0 +1,17 @@
+//
+//  passkeys_webview_embeddedApp.swift
+//  passkeys-webview-embedded
+//
+//  Created by Jan on 11.10.24.
+//
+
+import SwiftUI
+
+@main
+struct passkeys_webview_embeddedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
