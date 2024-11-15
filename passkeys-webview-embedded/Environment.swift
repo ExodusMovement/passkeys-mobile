3083232de62ad6447e8ece6f9be306d59581b5c0
andrejborstnik <andrej-borstnik@hotmail.com>
2024-11-15 10:13:48 +0100

    Revert "feat: test with passkeys playground"
    This reverts commit 9f22bdc89c7d2f5436cf4d43769e976aafb0d7ca.

--- passkeys-webview-embedded/Environment.swift
+++ passkeys-webview-embedded/Environment.swift
@@ -1,5 +1,5 @@
 import SwiftUI
 
 extension EnvironmentValues {
-    @Entry var embeddedWalletUrl: String = "https://passkeys.foundation/playground?relay&returnTo=passkeys%3A%2F%2F%0A"
+    @Entry var embeddedWalletUrl: String = "https://wallet-d.passkeys.foundation?relay&returnTo=passkeys%3A%2F%2F%0A"
 }
