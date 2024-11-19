db681fe3dd4e3ca028a1798a695fc6f3f68cfdca
andrejborstnik <andrej-borstnik@hotmail.com>
2024-11-19 14:35:56 +0100

    feat: get returnTo app signal from the wallet via relay instead

--- ios/passkeys-webview-embedded/Environment.swift
+++ ios/passkeys-webview-embedded/Environment.swift
@@ -1,5 +1,5 @@
 import SwiftUI
 
 extension EnvironmentValues {
-    @Entry var embeddedWalletUrl: String = "https://dev.passkeys.foundation/playground?relay&returnTo=passkeys%3A%2F%2F%0A"
+    @Entry var embeddedWalletUrl: String = "https://dev.passkeys.foundation/playground?relay"
 }
