366d6249245fcbaa91dd29f75fe5d505c8b4dc87
andrejborstnik <andrej-borstnik@hotmail.com>
2024-11-26 09:37:32 +0100

    refactor: onActivityResult to internal passkeys logic

--- example/android/app/src/main/java/com/passkeysandroid/MainActivity.kt
+++ example/android/app/src/main/java/com/passkeysandroid/MainActivity.kt
@@ -30,11 +30,6 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        passkeys.handleActivityResult(requestCode, resultCode)
-    }
-
     fun reopenMainActivity() {
         reopenMainActivityIntent?.send()
     }
