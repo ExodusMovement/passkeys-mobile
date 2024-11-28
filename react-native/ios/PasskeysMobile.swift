077cec0ff179feb433b83ca53bef34b479b7d531
andrejborstnik <andrej-borstnik@hotmail.com>
2024-11-28 16:45:38 +0100

    feat: expose js apis

--- react-native/ios/PasskeysMobile.swift
+++ react-native/ios/PasskeysMobile.swift
@@ -1,9 +1,164 @@
+import SafariServices
+import WebKit
 import SwiftUI
 
-public struct PasskeysMobile: View {
-    public init() {}
+class WebViewModel: ObservableObject {
+    @Published var webView: WKWebView? = nil
+}
 
-    public var body: some View {
-        ContentView()
+struct PasskeysMobile: View {
+    @Environment(\.embeddedWalletUrl) var embeddedWalletUrl: String
+    @ObservedObject var viewModel: WebViewModel
+
+
+    var body: some View {
+        var delegate: WebviewDelegate?
+        delegate = WebviewDelegate(openURLHandler: { url in
+            if let ctrl = RCTPresentedViewController() {
+                delegate?.presentSafariView(from: ctrl, url: url)
+            } else {
+                print("Failed to retrieve presented view controller.")
+            }
+        })
+
+        if let delegate = delegate {
+            return Webview(
+                url: URL(string: embeddedWalletUrl)!,
+                uiDelegate: delegate,
+                onWebViewCreated: { webView in
+                    self.viewModel.webView = webView
+                }
+            )
+            .ignoresSafeArea()
+            .navigationTitle("Passkeys")
+            .navigationBarTitleDisplayMode(.inline)
+        } else {
+            return Text("Error: Delegate not initialized")
+        }
+    }
+
+    public func callAsyncJavaScript(_ script: String, completion: @escaping (Result<Any, Error>) -> Void) {
+        guard let webviewInstance = viewModel.webView else {
+            print("test evaluating JS missing webview instance", viewModel.webView)
+            return
+        }
+
+        Task {
+            do {
+                let jsResult = try await webviewInstance.callAsyncJavaScript(
+                    script,
+                    arguments: [:],
+                    contentWorld: .page
+                )
+
+                completion(.success(jsResult))
+            } catch {
+                // Handle errors and propagate them to the completion handler
+                print("JavaScript execution failed:", error.localizedDescription)
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func callMethod(_ method: String, data: [String: Any]?, completion: @escaping (Result<Any, Error>) -> Void) {
+        let dataJSON: String
+        if let data = data,
+           let dataString = try? JSONSerialization.data(withJSONObject: data),
+           let jsonString = String(data: dataString, encoding: .utf8) {
+            dataJSON = jsonString
+        } else {
+            dataJSON = ""
+        }
+
+        let script = """
+        const result = window.\(method)(\(dataJSON));
+        if (result instanceof Promise) {
+            return result
+                .then(resolved => {
+                    console.log('Async Promise resolved:', resolved);
+                    return resolved; // Return resolved value directly
+                })
+                .catch(error => {
+                    console.error('Async Promise rejected:', error);
+                    throw error; // Throw error to propagate to Swift
+                });
+        } else {
+            console.log('Sync result:', result);
+            return result; // Return sync result directly
+        }
+        """
+        callAsyncJavaScript(script, completion: completion)
+    }
+}
+
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    let onDismiss: () -> Void
+
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.delegate = context.coordinator
+        return safariVC
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onDismiss: onDismiss)
+    }
+
+    class Coordinator: NSObject, SFSafariViewControllerDelegate {
+        let onDismiss: () -> Void
+
+        init(onDismiss: @escaping () -> Void) {
+            self.onDismiss = onDismiss
+        }
+
+        func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+            onDismiss()
+        }
+    }
+}
+
+class WebviewDelegate: NSObject, WKUIDelegate {
+    private var openURLHandler: (URL) -> Void
+    private weak var hostingController: UIViewController?
+
+    init(openURLHandler: @escaping (URL) -> Void) {
+        self.openURLHandler = openURLHandler
+    }
+
+    func presentSafariView(from ctrl: UIViewController, url: URL) {
+        let safariView = SafariView(
+            url: url,
+            onDismiss: {
+                ctrl.dismiss(animated: true)
+                self.hostingController = nil
+            }
+        )
+        let hostingController = UIHostingController(rootView: safariView)
+        self.hostingController = hostingController
+        ctrl.present(hostingController, animated: true)
+    }
+
+    func closeSafariView() {
+        hostingController?.dismiss(animated: true, completion: {
+            self.hostingController = nil
+        })
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        createWebViewWith configuration: WKWebViewConfiguration,
+        for navigationAction: WKNavigationAction,
+        windowFeatures: WKWindowFeatures
+    ) -> WKWebView? {
+        if let url = navigationAction.request.url {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.openURLHandler(url)
+            }
+        }
+        return nil
     }
 }
--- react-native/ios/Webview.swift
+++ react-native/ios/Webview.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct Webview: UIViewRepresentable {
     let url: URL
     let uiDelegate: WebviewDelegate
+    var onWebViewCreated: ((WKWebView) -> Void)?
 
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -24,17 +25,31 @@ struct Webview: UIViewRepresentable {
         window.uiControl.closeSigner = function() {
             window.webkit.messageHandlers.closeSigner.postMessage(null);
         };
+        const readyEvent = new Event('wallet-standard:app-ready')
+        readyEvent.detail = { register: (gui) => window.gui = gui }
+
+        const emitAppReady = () => window.dispatchEvent(readyEvent)
+        setTimeout(() => emitAppReady(), 3000)
         """
         contentController.addUserScript(WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false))
 
         let webView = WKWebView(frame: .zero, configuration: configuration)
         webView.uiDelegate = uiDelegate
+
         webView.load(URLRequest(url: url))
+
+        DispatchQueue.main.async {
+            self.onWebViewCreated?(webView)
+        }
 
         return webView
     }
 
-    func updateUIView(_ webView: WKWebView, context: Context) {}
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        DispatchQueue.main.async {
+            self.onWebViewCreated?(webView)
+        }
+    }
 
     class Coordinator: NSObject, WKScriptMessageHandler {
         var parent: Webview
