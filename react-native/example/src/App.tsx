6fa556da3f4f661c899c3377a04ceddc2e45c50a
andrejborstnik <andrej-borstnik@hotmail.com>
2024-12-13 14:59:50 +0100

    fix: example import

--- react-native/example/src/App.tsx
+++ react-native/example/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { SafeAreaView, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
 import {
-  ReactNativePasskeysView,
+  Passkeys,
   connect,
   signMessage,
 } from '@exodus/react-native-passkeys-sdk';
