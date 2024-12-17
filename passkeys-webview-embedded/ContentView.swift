eebae482beaa79bc2c66f659f5e9a1eec8abf136
Andrej <andrej-borstnik@hotmail.com>
2024-12-17 12:51:01 +0100

    feat: open signer in ASWebAuthenticationSession (#2)
    * feat: open the signer in the in-app browser
    
    * feat: support ios 15
    
    * feat: use ASWebAuthenticationSession
    
    * Revert "feat: support ios 15"
    
    This reverts commit 03697c816ff2c805c4f00d9a1d18236cb7facd2a.
    
    * feat: use dev server
    
    * feat: close auth on completion
    
    * Revert "feat: use ASWebAuthenticationSession"
    
    This reverts commit 2bd07908113d8703cd62f96d003c0e253c974c09.
    
    * Reapply "feat: use ASWebAuthenticationSession"
    
    This reverts commit c0538eac43b6f1c7da710b2086e9dbde1ba8456c.
    
    * feat: test with passkeys playground
    
    * gitignore
    
    * Revert "feat: test with passkeys playground"
    
    This reverts commit 9f22bdc89c7d2f5436cf4d43769e976aafb0d7ca.

--- passkeys-webview-embedded/ContentView.swift
+++ passkeys-webview-embedded/ContentView.swift
@@ -4,24 +4,59 @@
 //
 //  Created by Jan on 02.10.24.
 //
+import AuthenticationServices
 import WebKit
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.embeddedWalletUrl) var embeddedWalletUrl: String
+    @State private var showAuthSession = false
+    @State private var urlToOpen: URL?
+    private let authSessionCoordinator = AuthSessionCoordinator()
 
-       
-   var body: some View {
-       let delegate = WebviewDelegate()
-       
-       Webview(url: URL(string: embeddedWalletUrl)!, uiDelegate: delegate)
-           .ignoresSafeArea()
-           .navigationTitle("Passkeys")
-           .navigationBarTitleDisplayMode(.inline)
-   }
+    var body: some View {
+        let delegate = WebviewDelegate(openURLHandler: { url in
+            self.urlToOpen = url
+            self.showAuthSession = true
+            authSessionCoordinator.startAuthSession(url: url)
+        })
+
+        ZStack {
+            Webview(url: URL(string: embeddedWalletUrl)!, uiDelegate: delegate)
+                .ignoresSafeArea()
+                .navigationTitle("Passkeys")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+class AuthSessionCoordinator: NSObject, ASWebAuthenticationPresentationContextProviding {
+    private var authSession: ASWebAuthenticationSession?
+
+    func startAuthSession(url: URL) {
+        // todo switch over to https://developer.apple.com/documentation/authenticationservices/aswebauthenticationsession/init(url:callback:completionhandler:)
+        authSession = ASWebAuthenticationSession(url: url, callbackURLScheme: "passkeys") { callbackURL, error in
+            if let error = error {
+                print("Authentication failed with error: \(error.localizedDescription)")
+            } else if let callbackURL = callbackURL {
+                print("Authentication succeeded with callback URL: \(callbackURL)")
+            }
+        }
+        authSession?.presentationContextProvider = self
+        authSession?.start()
+    }
+
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return UIApplication.shared.windows.first { $0.isKeyWindow }!
+    }
 }
 
 class WebviewDelegate: NSObject, WKUIDelegate {
+    private var openURLHandler: (URL) -> Void
+
+    init(openURLHandler: @escaping (URL) -> Void) {
+        self.openURLHandler = openURLHandler
+    }
 
     func webView(
         _ webView: WKWebView,
@@ -29,15 +64,10 @@ class WebviewDelegate: NSObject, WKUIDelegate {
         for navigationAction: WKNavigationAction,
         windowFeatures: WKWindowFeatures
     ) -> WKWebView? {
-        // this is called on window.open
         if let url = navigationAction.request.url {
-            UIApplication.shared.open(url)
+            openURLHandler(url)
         }
         
         return nil
     }
-}
-
-#Preview {
-    ContentView()
 }
