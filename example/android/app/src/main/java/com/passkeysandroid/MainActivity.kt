d20ef519a13c6f62d7843097499a6e6042e3b167
andrejborstnik <andrej-borstnik@hotmail.com>
2024-12-19 12:51:56 +0100

    feat: basic android UI

--- example/android/app/src/main/java/com/passkeysandroid/MainActivity.kt
+++ example/android/app/src/main/java/com/passkeysandroid/MainActivity.kt
@@ -4,6 +4,8 @@ import foundation.passkeys.mobile.PasskeysMobile
 
 import android.app.PendingIntent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,14 +19,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rootLayout = findViewById<RelativeLayout>(R.id.root_layout)
-        passkeys = PasskeysMobile(this, null, 0, this)
 
-        val layoutParams = RelativeLayout.LayoutParams(
-            RelativeLayout.LayoutParams.MATCH_PARENT,
-            RelativeLayout.LayoutParams.MATCH_PARENT
-        )
-        passkeys.layoutParams = layoutParams
-
+        passkeys = PasskeysMobile(this, null, 0, this).apply {
+            visibility = View.GONE // Initially hide the Passkeys view
+            layoutParams = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT
+            )
+        }
+        passkeys.visibility = View.GONE
         rootLayout.addView(passkeys)
+
+        val connectButton = Button(this).apply {
+            text = "Connect"
+            layoutParams = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                addRule(RelativeLayout.CENTER_IN_PARENT)
+            }
+            setOnClickListener {
+                passkeys.callMethod("connect", null) { result ->
+                    result.fold(
+                        onSuccess = { println(it) },
+                        onFailure = { println(it) }
+                    )
+                }
+            }
+        }
+
+        rootLayout.addView(connectButton)
     }
 }
